c59e7c7c65980d5720794308292b01dbed8938e8
jakubdybczak <jakub.dybczak@gmail.com>
2020-10-24 18:17:10 +0200

    Add splashscreen

--- app/src/main/java/org/airella/airella/StartActivity.kt
+++ app/src/main/java/org/airella/airella/StartActivity.kt
@@ -3,32 +3,59 @@ package org.airella.airella
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.drawerlayout.widget.DrawerLayout
 import org.airella.airella.data.service.AuthService
 import org.airella.airella.data.service.PreferencesService
 import org.airella.airella.ui.login.LoginActivity
 import org.airella.airella.utils.Log
 
 class StartActivity : AppCompatActivity() {
-
+    var lanuched = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+
         PreferencesService.init(applicationContext)
 
-        setContentView(R.layout.activity_start)
+        val motionLayout: MotionLayout = findViewById(R.id.motion_layout)
+        val context = applicationContext;
+        motionLayout.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+
+            }
+
+            override fun onTransitionChange(
+                p0: MotionLayout?,
+                p1: Int,
+                p2: Int,
+                percentage: Float
+            ) {
+                if (percentage > 0.9 && !lanuched) {
+                    lanuched = true
+                    if (AuthService.isUserLogged()) {
+                        Log.i("User logged")
+                        val intent = Intent(context, MainActivity::class.java)
+                        intent.flags =
+                            Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                        startActivity(intent)
+                    } else {
+                        Log.i("User not logged")
+                        val intent = Intent(context, LoginActivity::class.java)
+                        intent.flags =
+                            Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                        startActivity(intent)
+                    }
+                }
+            }
+
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {}
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {}
+        })
     }
 
     override fun onResume() {
         super.onResume()
-        if (AuthService.isUserLogged()) {
-            Log.i("User logged")
-            val intent = Intent(this, MainActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-            startActivity(intent)
-        } else {
-            Log.i("User not logged")
-            val intent = Intent(this, LoginActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-            startActivity(intent)
-        }
     }
 }
