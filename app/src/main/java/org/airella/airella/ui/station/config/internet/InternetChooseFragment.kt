ab4bcba0746e58842d55e45ab60a7905b61336a3
Rafał <franczakr066@gmail.com>
2020-12-21 19:47:40 +0100

    Disable gsm for registration

--- app/src/main/java/org/airella/airella/ui/station/config/internet/InternetChooseFragment.kt
+++ app/src/main/java/org/airella/airella/ui/station/config/internet/InternetChooseFragment.kt
@@ -5,13 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import kotlinx.android.synthetic.main.fragment_internet_choose.*
 import org.airella.airella.R
+import org.airella.airella.ui.station.config.ConfigViewModel
 import org.airella.airella.ui.station.config.gsm.GsmConfigFragment
 import org.airella.airella.ui.station.config.wifi.WifiListFragment
 import org.airella.airella.utils.FragmentUtils.switchFragmentWithBackStack
 
 class InternetChooseFragment : Fragment() {
+
+    private val configViewModel: ConfigViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,9 +31,15 @@ class InternetChooseFragment : Fragment() {
             switchFragmentWithBackStack(R.id.container, WifiListFragment())
         }
 
-        gsm_button.setOnClickListener {
-            switchFragmentWithBackStack(R.id.container, GsmConfigFragment())
+        if (configViewModel.isWizard()) {
+            gsm_button.isEnabled = false
+            text_disabled_gsm.visibility = View.VISIBLE
+        } else {
+            gsm_button.setOnClickListener {
+                switchFragmentWithBackStack(R.id.container, GsmConfigFragment())
+            }
         }
+
     }
 
 }
